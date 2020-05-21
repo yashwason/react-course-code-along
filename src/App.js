1aa2fb62b0d3662d4de5100373bff9c4fee4d728
Yash Wason <yashwason@gmail.com>
2020-05-21 08:28:10 +0530

    Using CSS modules. Removed styled-components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
 
 import Person from './Person/Person';
-import './App.css';
+import styles from './App.css';
 
 
 class App extends Component{
@@ -44,24 +43,8 @@ class App extends Component{
     }
 
     render = () => {
-        const StyledButton = styled.button`
-            padding: 10px;
-            border-radius: 3px;
-            border: 1px solid #f00;
-            font-weight: 600;
-            font-size: 1.2em;
-            margin: 20px;
-            color: #fff;
-            background: ${ (props) => props.showPersons ? `#f00` : `rgb(10, 156, 10)` };
-            transition: all 0.25s;
-            
-            &:hover{
-                background: ${ (props) => props.showPersons ? `salmon` : `rgb(30, 185, 30)` };
-                transform: scale(1.05);
-            }
-        `;
-        
-        let persons = null;
+        let persons = null,
+            appliedStyles = [];
         if(this.state.showPersons){
             persons = (
                 <div>
@@ -75,15 +58,17 @@ class App extends Component{
                     }) }
                 </div>
             );
+
+            appliedStyles.push(styles.clicked);
         }
 
         return (
-            <div className="App">
+            <div className={ styles.App }>
                 <h1>Yash's First React Code</h1>
                 <p>This is a paragraph written in JSX</p>
 
-                <StyledButton key="45asd" showPersons={ this.state.showPersons } onClick={() => this.switchNameHandler(`Yash`)}>Switch Names</StyledButton>
-                <StyledButton key="465dg" showPersons={ this.state.showPersons } onClick={ this.togglePersonsHandler }>Toggle Persons</StyledButton>
+                <button className={ appliedStyles.join(` `) } key="45asd" onClick={() => this.switchNameHandler(`Yash`)}>Switch Names</button>
+                <button className={ appliedStyles.join(` `) } key="465dg" onClick={ this.togglePersonsHandler }>Toggle Persons</button>
 
                 { persons }
             </div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,37 +1,17 @@
 import React from 'react';
-import styled from 'styled-components';
+import styles from './Person.css';
 
 const Person = (props) => {
-    const StyledDiv = styled.div`
-        width: 40%;
-        max-width: 500px;
-        border: 1px solid #000;
-        border-radius: 5px;
-        padding: 30px;
-        text-align: center;
-        margin: 30px auto 0;
-        box-shadow: 2px 2px 2px #000;
-
-        *{
-            display: block;
-            margin: 15px auto;
-        }
-
-        @media (max-width: 500px){
-            width: 80%;
-        }
-    `;
-
     return (
-        <StyledDiv>
+        <div className={ styles.Person }>
             <p onClick={ props.click }>My name is { props.name } and I am { props.age } years old.</p>
             <strong>{ props.children }</strong>
 
             <input type="text" onChange={ props.changeNameHandler } value={ props.name }/>
 
             <button onClick={ props.removeComponentHandler }>Remove Person</button>
-        </StyledDiv>
-    )
+        </div>
+    );
 }
 
 export default Person;
