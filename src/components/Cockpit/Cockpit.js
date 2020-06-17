a1de0e4c9295e30f84c9345bd76921e2a3e717e5
Yash Wason <yashwason@gmail.com>
2020-06-17 22:35:07 +0530

    Returning adjacent JSX elements in Cockpkit.js using custom Auxiliary element

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import Aux from '../../hoc/Auxiliary';
 import styles from './Cockpit.css';
 
 const Cockpit = (props) => {
@@ -11,23 +12,22 @@ const Cockpit = (props) => {
 
     if(props.showPersons) appliedStyles.push(styles.clicked);
 
-    let optionalMarkup =
-        <div>
+    let optionalMarkup = <Aux>
             <h1>{ props.title }</h1>
             <p>Manage persons using this React App</p>
 
             <button className={ appliedStyles.join(` `) } key="45asd" onClick={ props.switchNameHandler }>Switch Names</button>
             <button className={ appliedStyles.join(` `) } key="465dg" onClick={ props.togglePersonsHandler }>Toggle Persons</button>
-        </div>
+        </Aux>;
 
 
     if(!props.showCockpit) optionalMarkup = null;
 
     return (
         <div className={ styles.Cockpit }>
-            <button className={ appliedStyles.join(` `) } key="a98ad" onClick={ props.toggleCockpit }>Toggle Cockpit</button>
-
             { optionalMarkup }
+            
+            <button className={ appliedStyles.join(` `) } key="a98ad" onClick={ props.toggleCockpit }>Toggle Cockpit</button>
         </div>
     );
 };
